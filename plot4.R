b0bb24f956e5f8344ab4273f7f618684b7c754b0
Date A. Scientist <dateascientist@gmail.com>
2015-04-12 08:19:25 -0600

    Fixed legend in 4th output PNG

--- plot4.R
+++ plot4.R
@@ -50,8 +50,8 @@ with(dataByDate, lines(dataByDate$datetime, dataByDate$Sub_metering_2,
         col = "red"))
 with(dataByDate, lines(dataByDate$datetime, dataByDate$Sub_metering_3, 
         col = "blue"))
-legend("topright", col = c("black", "red", "blue"),lty = c(1, 1, 1), 
-       lwd = c(1, 1, 1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", bty = "n", col = c("black", "red", "blue"),lty = c(1, 1, 1), 
+        lwd = c(1, 1, 1), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Plot 4th
 with(dataByDate, plot(dataByDate$datetime, dataByDate$Global_reactive_power, 
